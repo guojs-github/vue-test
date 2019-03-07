80f5dbecc355437eaa332f4b171499962b736487
guojs-github <guoojs@gmail.com>
2019-03-07 09:57:00 +0800

    20190307 jest timer test

---
+++ src/assets/util/timer.js
@@ -0,0 +1,25 @@
+function timerGame(callback) {
+  console.log('Ready....go!');
+  setTimeout(() => {
+    console.log('Times up -- stop!');
+    callback && callback();
+  }, 10000);
+}
+
+function infiniteTimerGame(callback) {
+  console.log('Ready....go!');
+
+  setTimeout(() => {
+    console.log('Times up! 10 seconds before the next game starts...');
+    callback && callback();
+
+    // Schedule the next game in 10 seconds
+    setTimeout(() => {
+      infiniteTimerGame(callback);
+    }, 10000);
+  }, 1000);
+}
+module.exports = {
+	timerGame,
+	infiniteTimerGame
+};
---
+++ src/assets/util/timer.test.js
@@ -0,0 +1,58 @@
+beforeEach(() => { 
+	jest.useFakeTimers(); // 此处重置时钟相关计数器
+}); 
+
+test('waits 10 second before ending the game', () => {
+	const { timerGame } = require('./timer');
+	const callback = jest.fn();
+	timerGame(callback);
+
+	expect(callback).not.toBeCalled();
+
+	jest.runAllTimers();
+	
+	expect(callback).toBeCalled();
+	expect(callback).toHaveBeenCalledTimes(1);
+});
+
+describe('infiniteTimerGame', () => {
+	test('schedules a 10-second timer after 1 second', () => {
+		const { infiniteTimerGame } = require('./timer');
+		const callback = jest.fn();
+
+		infiniteTimerGame(callback);
+
+		// At this point in time, there should have been a single call to
+		// setTimeout to schedule the end of the game in 1 second.
+		expect(setTimeout).toHaveBeenCalledTimes(1);
+		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+
+		// Fast forward and exhaust only currently pending timers
+		// (but not any new timers that get created during that process)
+		jest.runOnlyPendingTimers();
+
+		// At this point, our 1-second timer should have fired it's callback
+		expect(callback).toBeCalled();
+
+		// And it should have created a new timer to start the game over in
+		// 10 seconds
+		expect(setTimeout).toHaveBeenCalledTimes(2);
+		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 10000);
+	});
+});
+
+test('waits 60 second & count times', () => {
+	const { infiniteTimerGame } = require('./timer');
+	const callback = jest.fn();
+
+	infiniteTimerGame(callback);
+
+	expect(setTimeout).toHaveBeenCalledTimes(1);
+	expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+
+	// Fast forward 1 minute
+	jest.advanceTimersByTime(60000);
+
+	expect(callback).toBeCalled();
+	expect(callback).toHaveBeenCalledTimes(6);
+});
