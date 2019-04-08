e3dedb3e3ecc83108c8965c41fed0ac6d2801ed9
guojs-github <guoojs@gmail.com>
2019-04-08 09:40:35 +0800

    20190408 Jest configuring

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -1,32 +1,33 @@
 const path = require('path')
 
 module.exports = {
-  rootDir: path.resolve(__dirname, '../../'),
-  moduleFileExtensions: [
-    'js',
-    'json',
-    'vue'
-  ],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
-  transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
-  },
-  testPathIgnorePatterns: [
-    '<rootDir>/test/e2e'
-  ],
-  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
-  setupFiles: ['<rootDir>/test/unit/setup'],
-  coverageDirectory: '<rootDir>/test/unit/coverage',
-  collectCoverageFrom: [
-    'src/**/*.{js,vue}',
-    '!src/main.js',
-    '!src/router/index.js',
-    '!**/node_modules/**'
-  ],
-  testURL: 'http://localhost',
-  verbose: false,
-  collectCoverage: false
+	displayName: 'Vue Jest 测试验证',
+	rootDir: path.resolve(__dirname, '../../'),
+	moduleFileExtensions: [
+		'js',
+		'json',
+		'vue'
+	],
+	moduleNameMapper: {
+		'^@/(.*)$': '<rootDir>/src/$1'
+	},
+	transform: {
+		'^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+		'.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+	},
+	testPathIgnorePatterns: [
+		'<rootDir>/test/e2e'
+	],
+	snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+	setupFiles: ['<rootDir>/test/unit/setup'],
+	coverageDirectory: '<rootDir>/test/unit/coverage',
+	collectCoverageFrom: [
+		'src/**/*.{js,vue}',
+		'!src/main.js',
+		'!src/router/index.js',
+		'!**/node_modules/**'
+	],
+	testURL: 'http://localhost',
+	verbose: false,
+	collectCoverage: true
 }
