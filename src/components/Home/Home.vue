74ea82ee3aabc96b9f138b1f0ee098550adefea8
guojs-github <guoojs@gmail.com>
2020-12-10 11:59:33 +0800

    重构首页

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class='home'>
+		你好！我是Home
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Home', 
+	
+	components: {
+	},
+	
+	data() {
+		return {
+		};
+	},
+
+	created() {
+		console.log('Home created');
+		
+		this.init();
+	},
+
+	mounted () {
+		console.log('On mounted home page.');
+	},
+	
+	beforeDestroy() {
+		console.log('On before destory home page.');
+
+		this.uninit();
+	},
+
+	methods: {		
+		/*************************/
+		init() {
+			console.log('Initialize.');
+		},
+
+		uninit() {
+			console.log('Uninitialize.');
+		}
+	}
+};
+</script>
+
+<style scoped src='./Home.less' lang='less'/>
+<style scoped>
+</style>
